099f28e6f50862c9bcc20dacaf04fc8779bb82e0
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-06-19 00:29:36 +0200

    Mount some external paths

--- wke-mkenv.py
+++ wke-mkenv.py
@@ -6,6 +6,7 @@ from docker import Client
 WKE_IMAGE = sys.argv[2]
 WKE_ENV = sys.argv[1]
 WKE_PREFIX = "wke"
+WKE_BINDS = ["/home/jasuarez/Container/:/home/jasuarez/external:rw"]
 
 cli = Client()
 
@@ -14,7 +15,14 @@ fullenv = WKE_PREFIX + "-" + WKE_ENV
 
 envvars = {"DOCKER_NAME": WKE_ENV}
 
-result = cli.create_container(image=fullimg, hostname=WKE_IMAGE, stdin_open=True, tty=True, name=fullenv, environment=envvars)
+result = cli.create_container(image = fullimg,
+                              hostname = WKE_IMAGE,
+                              host_config = cli.create_host_config(binds = WKE_BINDS,
+                                                                   privileged = True),
+                              stdin_open = True,
+                              tty = True,
+                              name = fullenv,
+                              environment = envvars)
 for s in result:
     print(s)
 
--- wke-once.py
+++ wke-once.py
@@ -8,6 +8,7 @@ from docker import Client
 
 WKE_NAME = sys.argv[1]
 WKE_PREFIX = "wke"
+WKE_BINDS = ["/home/jasuarez/Container/:/home/jasuarez/external:rw"]
 
 fullenv = WKE_PREFIX + "-" + WKE_NAME
 fullimg = WKE_PREFIX + "/" + WKE_NAME
@@ -20,7 +21,7 @@ if len(containers) > 0:
     image = containers[0]['Image']
     hostname = image[len(WKE_PREFIX):] + "/" + timestamp
     clone = cli.commit(container=fullenv, repository=image, tag=timestamp);
-    call(["docker", "run", "-t", "-i", "--rm", "-h", hostname, clone['Id']])
+    call(["docker", "run", "--privileged", "-t", "-i", "--rm", "-h", hostname, "-v", WKE_BINDS[0], clone['Id']])
     cli.remove_image(clone['Id'])
 else:
     images = cli.images()
@@ -28,4 +29,4 @@ else:
         tag = i['RepoTags']
         if tag[0] == fullimg + ":latest":
             hostname = WKE_NAME + "/" + timestamp
-            call(["docker", "run", "-t", "-i", "--rm", "-h", hostname, i['Id']])
+            call(["docker", "run", "--privileged", "-t", "-i", "--rm", "-h", hostname, "-v", WKE_BINDS[0], i['Id']])
