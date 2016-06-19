d825b1e94471383341137c39cac3d736f77c48cb
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-06-19 02:20:14 +0200

    Add mkimg command
    Creates image from Dockerfile.

---
+++ src/wke/commands/mkimg.py
@@ -0,0 +1,21 @@
+class Command(object):
+    def __init__(self, docker_client, config):
+        self.docker_client = docker_client
+        self.config = config
+
+    def expected_params(self):
+        return 1
+
+    def usage(self):
+        return "<image>"
+
+    def run(self, image):
+        url = self.config.get_source_image(image)
+        cimage = self.config.get_canonical_image(image)
+        result = self.docker_client.build(path = url,
+                                          tag = cimage,
+                                          pull = True,
+                                          decode = True)
+        for s in result:
+            for k,v in s.items():
+                print(v)
