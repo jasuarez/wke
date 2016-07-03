57df4a0134c56409403db13089b4a434d0534ddd
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-07-04 01:52:02 +0200

    Add configuration file
    All the options will be read from this file.
    
    In future it will be possible to have different files.

--- src/wke/config.py
+++ src/wke/config.py
@@ -1,21 +1,25 @@
-_WKE_PREFIX = "wke"
+from xdg.BaseDirectory import xdg_config_dirs
+import os.path
+import ConfigParser
+
 _WKE_URL = "https://raw.githubusercontent.com/jasuarez/dockerfiles/wke/wke"
 
 class Config(object):
     def __init__(self):
-        self._WKE_PREFIX = "wke"
-        self._WKE_URL = "https://raw.githubusercontent.com/jasuarez/dockerfiles/wke/wke"
         self._WKE_BINDS = ["/home/jasuarez/Container/:/home/jasuarez/external:rw"]
+        self._config = ConfigParser.SafeConfigParser({"prefix": "wke"})
+        self._config.read([os.path.join(c, "wke", "defaults.conf")
+                           for c in xdg_config_dirs])
 
-        
     def get_canonical_image(self, image):
-        return _WKE_PREFIX + "/" + image
+        return self._config.get('globals', 'prefix') + "/" + image
 
     def get_canonical_env(self, env):
-        return _WKE_PREFIX + "-" + env
+        return self._config.get('globals', 'prefix') + "-" + env
 
     def get_source_image(self, image):
-        return _WKE_URL + "/" + image + "/Dockerfile"
+        images = self._config.get('globals', 'images')
+        return images + "/" + image + "/Dockerfile"
 
     def get_mount_points(self):
         return self._WKE_BINDS
